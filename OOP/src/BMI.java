33837549eeb3ead1942d4941afbac214a2993fd0
User <User@DESKTOP-414SI6L>
2024-01-23 20:28:44 +0100

    Krijimi i klases BMI.

---
+++ OOP/src/BMI.java
@@ -0,0 +1,35 @@
+
+public class BMI {
+private String name;
+private int mosha;
+private double pesha;
+private double gjatesia;
+
+/**krijon nje objekt te klases BMI me nje emer, moshe,peshe,gjatesi specifike
+ * @param name Emri i personit te cilit do i llogarisim BMI
+ * @param mosha Mosha e personit
+ * @param pesha Pesha ne kg e personit
+ * @param gjatesia Gjatesia ne metra e nje personi.
+ */
+public BMI(String name,int mosha,double pesha,double gjatesia) {
+	
+}
+/**
+ * 
+ * @return
+ */
+public double getBMI()  {
+	//to do
+	return 0;
+}
+/**
+ * 
+ * @return statusin ne baze te BMI psh normal,obez etj
+ */
+public String getStatus() {
+	//to do
+	return "";
+}
+	
+
+}
